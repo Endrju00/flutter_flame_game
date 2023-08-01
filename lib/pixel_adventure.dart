fcfba7337a31575494e4029473131e8687cdaf21
Endrju00 <andrzej.kapczynski21@gmail.com>
2023-08-01 20:50:52 +0200

    Add first level

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,28 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
-class PixelAdventure extends FlameGame {}
+import 'levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
+  late final CameraComponent cam;
+
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
