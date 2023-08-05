2dde9ce9abd36f53c47f518ddb0d9326d0f3744b
Endrju00 <andrzej.kapczynski21@gmail.com>
2023-08-05 13:10:29 +0200

    Add loading images

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -12,17 +12,22 @@ class PixelAdventure extends FlameGame {
 
   late final CameraComponent cam;
 
-  final world = Level();
+  final world = Level(levelName: 'Level-02');
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
       world: world,
       width: 640,
       height: 360,
     );
     cam.viewfinder.anchor = Anchor.topLeft;
+
     addAll([cam, world]);
+
     return super.onLoad();
   }
 }
