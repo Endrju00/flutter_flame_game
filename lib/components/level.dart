8cccb598d20302db41afafdce2ed52fc8268ab5f
Endrju00 <andrzej.kapczynski21@gmail.com>
2023-08-05 15:55:47 +0200

    Move files to other directory

--- lib/components/level.dart
+++ lib/components/level.dart
@@ -1,13 +1,15 @@
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
-import '../actors/player.dart';
+import 'player.dart';
 
 class Level extends World {
   final String levelName;
+  final Player player;
 
   Level({
     required this.levelName,
+    required this.player,
   });
 
   late TiledComponent level;
@@ -22,10 +24,7 @@ class Level extends World {
     for (final spawnPoint in spawnPointsLayer!.objects) {
       switch (spawnPoint.class_) {
         case 'Player':
-          final player = Player(
-            character: 'Ninja Frog',
-            position: Vector2(spawnPoint.x, spawnPoint.y),
-          );
+          player.position = Vector2(spawnPoint.x, spawnPoint.y);
           add(player);
           break;
         default:
