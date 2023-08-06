b8bf6aeb681dcdffcd8412036accd6d1059f8fd0
Endrju00 <andrzej.kapczynski21@gmail.com>
2023-08-06 22:55:03 +0200

    Update function

--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -1,19 +1,22 @@
-bool checkCollision(block1, block2) {
-  final hitbox = block1.hitbox;
-  final block1X = block1.position.x + hitbox.offsetX;
-  final block1Y = block1.position.y + hitbox.offsetY;
+import 'collision_block.dart';
+import 'player.dart';
+
+bool checkCollision(Player player, CollisionBlock block) {
+  final hitbox = player.hitbox;
+  final block1X = player.position.x + hitbox.offsetX;
+  final block1Y = player.position.y + hitbox.offsetY;
   final block1Width = hitbox.width;
   final block1Height = hitbox.height;
 
-  final blockX = block2.x;
-  final blockY = block2.y;
-  final blockWidth = block2.width;
-  final blockHeight = block2.height;
+  final blockX = block.x;
+  final blockY = block.y;
+  final blockWidth = block.width;
+  final blockHeight = block.height;
 
-  final fixedX = block1.scale.x < 0
+  final fixedX = player.scale.x < 0
       ? block1X - (hitbox.offsetX * 2) - block1Width
       : block1X;
-  final fixedY = block2.isPlatform ? block1Y + block1Height : block1Y;
+  final fixedY = block.isPlatform ? block1Y + block1Height : block1Y;
 
   return (fixedY < blockY + blockHeight &&
       block1Y + block1Height > blockY &&
