d7d6f418f880d19a730ff15c17d9f74094b98c17
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-01-14 20:09:31 +0100

    feat: Adjust game over screen

--- lib/game/core/presentation/screens/game_over.dart
+++ lib/game/core/presentation/screens/game_over.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_flame_game/game/features/points/bloc/score/score_bloc.dart';
-import 'package:flutter_flame_game/game/pixel_adventure.dart';
 
 import '../../../../injection_container.dart';
+import '../../../features/player/bloc/health/health_bloc.dart';
+import '../../../features/points/bloc/score/score_bloc.dart';
+import '../../../pixel_adventure.dart';
 import '../widgets/pixel_button.dart';
 import 'menu.dart';
 
 class GameOverScreen extends StatelessWidget {
   final PixelAdventure game;
+  final bool isPlayerDead;
 
   const GameOverScreen({
     super.key,
     required this.game,
+    required this.isPlayerDead,
   });
 
   @override
@@ -23,26 +26,47 @@ class GameOverScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Congratulations!',
-              style: TextStyle(
-                fontFamily: 'PixelifySans',
-                fontSize: 64,
-                color: Colors.white,
-              ),
-            ),
+            isPlayerDead
+                ? const Text(
+                    'Game over',
+                    style: TextStyle(
+                      fontFamily: 'PixelifySans',
+                      fontSize: 64,
+                      color: Colors.white,
+                    ),
+                  )
+                : const Text(
+                    'Congratulations!',
+                    style: TextStyle(
+                      fontFamily: 'PixelifySans',
+                      fontSize: 64,
+                      color: Colors.white,
+                    ),
+                  ),
             const SizedBox(height: 16),
-            const Text(
-              'You have completed the game.',
-              style: TextStyle(
-                fontFamily: 'PixelifySans',
-                fontSize: 32,
-                color: Colors.white,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
+            isPlayerDead
+                ? const Text(
+                    'You have lost all your hearts.',
+                    style: TextStyle(
+                      fontFamily: 'PixelifySans',
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  )
+                : const Text(
+                    'You have completed the game.',
+                    style: TextStyle(
+                      fontFamily: 'PixelifySans',
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
             BlocBuilder<ScoreBloc, ScoreState>(
+              bloc: sl<ScoreBloc>(),
               builder: (context, state) {
+                
                 return Text(
                   'Score: ${state.score}',
                   style: const TextStyle(
@@ -65,6 +89,7 @@ class GameOverScreen extends StatelessWidget {
               ),
               onPressed: () {
                 sl<ScoreBloc>().add(ResetScoreEvent());
+                sl<HealthBloc>().add(ResetHealthEvent());
                 Navigator.of(context).pushReplacement(
                   MaterialPageRoute(
                     builder: (context) => const Menu(),
