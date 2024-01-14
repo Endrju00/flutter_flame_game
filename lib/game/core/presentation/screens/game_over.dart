90b0215c2a2af47a685fd6de6d42bc3a88f89f1d
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-01-14 20:24:26 +0100

    fix: Fix overlay bug

--- lib/game/core/presentation/screens/game_over.dart
+++ lib/game/core/presentation/screens/game_over.dart
@@ -70,7 +70,6 @@ class GameOverScreen extends StatelessWidget {
                     builder: (context) => const Menu(),
                   ),
                 );
-                game.overlays.remove('Game over');
               },
             ),
           ],
