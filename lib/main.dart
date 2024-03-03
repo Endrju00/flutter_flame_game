140483328d7dafc29903b8270b8d1b7b7ebc9974
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-03-03 15:28:00 +0100

    fix: Fix bugs on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flame/flame.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'injection_container.dart' as di;
 import 'app.dart';
 
 Future<void> _setLandscape() async {
+  if (kIsWeb) return;
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
 }
