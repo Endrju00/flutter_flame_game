3b6c0d662e7424f47c2152f2ef20b49775182770
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-03-12 14:15:03 +0100

    feat: Add launching url

--- lib/game/core/presentation/screens/menu.dart
+++ lib/game/core/presentation/screens/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../injection_container.dart';
 import '../../../features/benchmark/bloc/benchmark_bloc.dart';
@@ -21,48 +23,81 @@ class Menu extends StatelessWidget {
     _navigateToGamePlay(context);
   }
 
+  void _lauchDeveloperSite() async {
+    if (!await launchUrl(Uri.parse('https://github.com/Endrju00'))) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF211F30),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Pixel Adventure',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'PixelifySans',
-                  fontSize: 64,
-                  color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            const Text(
+              'Pixel Adventure',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'PixelifySans',
+                fontSize: 64,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 32),
+            PixelButton(
+              text: 'Play',
+              icon: Image.asset(
+                'assets/images/Menu/Buttons/Play.png',
+                height: 24,
+                width: 24,
+                fit: BoxFit.cover,
+              ),
+              onPressed: () => _navigateToGamePlay(context),
+            ),
+            const SizedBox(height: 16),
+            PixelButton(
+              text: 'Benchmark',
+              icon: Image.asset(
+                'assets/images/Menu/Buttons/Settings.png',
+                height: 24,
+                width: 24,
+                fit: BoxFit.cover,
+              ),
+              onPressed: () => _enableBenchmark(context),
+            ),
+            const Spacer(),
+            InkWell(
+              onTap: _lauchDeveloperSite,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 320),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Game made by',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'PixelifySans',
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      SvgPicture.asset(
+                        'assets/logo-white.svg',
+                        height: 32,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 32),
-              PixelButton(
-                text: 'Play',
-                icon: Image.asset(
-                  'assets/images/Menu/Buttons/Play.png',
-                  height: 24,
-                  width: 24,
-                  fit: BoxFit.cover,
-                ),
-                onPressed: () => _navigateToGamePlay(context),
-              ),
-              const SizedBox(height: 16),
-              PixelButton(
-                text: 'Benchmark',
-                icon: Image.asset(
-                  'assets/images/Menu/Buttons/Settings.png',
-                  height: 24,
-                  width: 24,
-                  fit: BoxFit.cover,
-                ),
-                onPressed: () => _enableBenchmark(context),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
